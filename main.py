ac446de07acbf92b391817e95a5b839f81307574
Goutham Kumar Adunuri <77602631+Goutham6172@users.noreply.github.com>
2025-07-13 09:32:45 +0530

    updated the rate of sending data

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # tcp_server.py
 import socket, struct, time
 
-s = socket.socket()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("0.0.0.0", 5000))
 s.listen(1)
-conn, _ = s.accept()
+
+print("[TCP] Waiting for connection...")
+conn, addr = s.accept()
+print(f"[TCP] Connected by {addr}")
 
 while True:
     ts = int(time.time() * 1000)
     for r, az in [(100, 0), (120, 45), (140, -30)]:
         conn.sendall(struct.pack('ffQ', r, az, ts))
-    time.sleep(1)
+    time.sleep(1/20)
